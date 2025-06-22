9ed5842c1e2f8240934a50b972b6eb11536bf5a7
Gagandeep7002 <gagandeepsinghjass888@gmail.com>
2025-06-22 13:55:41 +0530

    add datatypes and variable files

---
+++ Basic codes/dataTypes.java
@@ -0,0 +1,18 @@
+class dataTypes {
+    public static void main(String[] args) {
+        //integer values 
+        byte a=50;//smaller range
+        short b=1400;//larger range than byte
+        int c=16126762;//larger range than short
+        long d=612766736667198924L;//largest range
+
+        //decimal values
+        float e=3.1415F;//smaller range
+        double f=1.2335672723;//larger range
+
+        //characters
+        char g='a';
+
+        System.out.println("Values of a,b,c,d,e,f,g respectively "+a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" ");
+        }
+}
---
+++ Basic codes/variables.java
@@ -0,0 +1,6 @@
+class variables {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println("I am "+ age +" years old");
+    }
+}
