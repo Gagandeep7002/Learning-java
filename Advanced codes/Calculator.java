8c23be9bbbbeec5a8d67c4e4194f5b9cb6710566
Gagandeep7002 <gagandeepsinghjass888@gmail.com>
2025-06-28 13:57:17 +0530

    update calculator file

--- Advanced codes/Calculator.java
+++ Advanced codes/Calculator.java
@@ -3,34 +3,39 @@ import java.util.Scanner;
 class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the operation you want to perform(+,-,*,/,%,power): ");
-        String op = scanner.nextLine();
-
-        System.out.print("Enter the first number: ");
-        double a = scanner.nextDouble();
-        
-        System.out.print("Enter the second number: ");
-        double b = scanner.nextDouble();
-
-        if (op.equals("+")){
-            System.out.printf("%f + %f = %f",a,b,a+b);
-        }else if (op.equals("-")){
-            System.out.printf("%f - %f = %f",a,b,a-b);
-        }else if (op.equals("*")){
-            System.out.printf("%f * %f = %f",a,b,a*b);
-        }else if (op.equals("/")){
-            if (b==0){
-                System.out.println("Cannot divide by zero");
-            }else{
-                System.out.printf("%f / %f = %f",a,b,a/b);
+        while (true){
+            System.out.print("\nEnter the operation you want to perform(+,-,*,/,%,power,exit): ");
+            String op = scanner.nextLine();
+            if (op.equalsIgnoreCase("exit")){
+                break;
             }
-        }else if (op.equals("%")){
-            System.out.printf("%f remainder %f = %f",a,b,a%b);
-        }else if (op.equals("power")){
-            System.out.printf("%f^%f = %f",a,b,Math.pow(a , b));
-        }
 
+            System.out.print("Enter the first number: ");
+            double a = scanner.nextDouble();
+        
+            System.out.print("Enter the second number: ");
+            double b = scanner.nextDouble();
+            scanner.nextLine();
+            if (op.equals("+")){
+                System.out.printf("%f + %f = %f",a,b,a+b);
+            }else if (op.equals("-")){
+                System.out.printf("%f - %f = %f",a,b,a-b);
+            }else if (op.equals("*")){
+                System.out.printf("%f * %f = %f",a,b,a*b);
+            }else if (op.equals("/")){
+                if (b==0){
+                    System.out.println("Cannot divide by zero");
+                }else{
+                    System.out.printf("%f / %f = %f",a,b,a/b);
+                }
+            }else if (op.equals("%")){
+                System.out.printf("%f remainder %f = %f",a,b,a%b);
+            }else if (op.equalsIgnoreCase("power")){
+                System.out.printf("%f^%f = %f",a,b,Math.pow(a , b));
+            }else{
+                System.out.println("Invalid operator");
+            }
+        }
         scanner.close();
     }
 }
